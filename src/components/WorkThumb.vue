52f287a4df272d3e1e4b7541484695dcc1c718fe
Nathalya Amorim <nathalyamorim87@gmail.com>
2023-02-19 22:10:18 -0500

    Adding work route. Fixing skill and work section.

--- src/components/WorkThumb.vue
+++ src/components/WorkThumb.vue
@@ -1,50 +1,19 @@
-<!-- export default {
-    name: "workthumb",
-
-    emits: ['load-light-box-data'],
-
-    props: {
-        work: Object
-    },
-
-    template: 
-    `
-    <div class="work-picture" @click="onWorkSelected">
-        <div class="translated-border">
-        </div>
-        <div class="work-picture-overlay">
-        </div>
-        <picture class="flex-center picture-filter">
-            <source media="(min-width:740px)" :srcset="work['project_thumb_img']">
-            <source media="(min-width:480px)" :srcset="work['project_thumb_img']">
-            <img class="profile-pic" :src="work['project_thumb_img']" :alt='work["project_name"]'>
-        </picture>
-            <span class="work-title">{{work['project_name']}}</span>
-    </div>
-    `,
-
-    methods: {
-        onWorkSelected() {
-            this.$emit('load-light-box-data', this.work);
-        }
-    }
-
-} -->
-
-
 <template>
-    <div class="work-picture" @click="onWorkSelected">
-        <div class="translated-border">
+    <router-link :to="'/work/' + work.project_id">
+        <div class="work-picture" @click="onWorkSelected" :data-aos="work.project_id % 2 === 0 ? 'fade-left' : 'fade-right'"
+            data-aos-ease="ease" data-aos-duration="500" data-aos-delay="500">
+            <div class="translated-border">
+            </div>
+            <span class="work-title">{{ work['project_name'] }}</span>
+            <div class="work-picture-overlay">
+            </div>
+            <picture class="flex-center picture-filter">
+                <source media="(min-width:740px)" :srcset="work['project_thumb_img']">
+                <source media="(min-width:480px)" :srcset="work['project_thumb_img']">
+                <img class="profile-pic" :src="work['project_thumb_img']" :alt='work["project_name"]'>
+            </picture>
         </div>
-        <div class="work-picture-overlay">
-        </div>
-        <picture class="flex-center picture-filter">
-            <source media="(min-width:740px)" :srcset="work['project_thumb_img']">
-            <source media="(min-width:480px)" :srcset="work['project_thumb_img']">
-            <img class="profile-pic" :src="work['project_thumb_img']" :alt='work["project_name"]'>
-        </picture>
-        <span class="work-title">{{ work['project_name'] }}</span>
-    </div>
+    </router-link>
 </template>
 
 <script>
@@ -64,6 +33,4 @@ export default {
 }
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
---
+++ src/views/WorkView.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'WorkView',
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    }
+}
+</script>
+
+<template>
+    <main id="body">
+        {{ id }}
+    </main>
+</template>
+<style lang="scss">
+main {
+    max-width: 1280px;
+    margin: auto;
+}
+</style>
